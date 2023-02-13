dd9d9b37d50ece049f1a42982fc37afe0aadfaaa
francesco <info.francescobianco@gmail.com>
2023-02-13 20:20:39 +0100

    First commit

--- public/google.php
+++ public/google.php
@@ -7,3 +7,7 @@ $db = App\GlobalSheetbase::getInstance();
 $db->setDatabase('global');
 $db->setTable('table1');
 var_dump($db->get(0, 0));
+
+
+?>
+<iframe width="100%" height="400" src="https://docs.google.com/spreadsheets/d/1-SQHS23HPxNoEXSss7nAMShZMz5wwM8VAd8o16X6WRs/edit?userstoinvite=sheetbase-demo@sheetbase-demo.iam.gserviceaccount.com#gid=0"></iframe>
